bc67fa99b25a7a75f298ce03d59f38467eaf94d5
Arter Tendean <arter.tendean.07@gmail.com>
2025-03-23 19:34:02 +0700

    Refactor module templates with neubrutalism design; update authentication links and enforce superuser access for module management (#10)

--- modules/views.py
+++ modules/views.py
@@ -1,4 +1,8 @@
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import UserPassesTestMixin
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
@@ -9,18 +13,41 @@ from django.views.generic import ListView
 from modules.models import Module
 
 
+@login_required(login_url="/admin/login/")
+@user_passes_test(lambda u: u.is_superuser)
 def index(request):
     return HttpResponse("Module index page")
 
 
-class ModuleListView(ListView):
+class ModuleListView(LoginRequiredMixin, UserPassesTestMixin, ListView):
     model = Module
     template_name = "modules/module_list.html"
     context_object_name = "modules"
+    login_url = "/admin/login/"  # Uses Django admin login
+    redirect_field_name = "next"
+
+    def test_func(self):
+        return self.request.user.is_superuser
+
+    def handle_no_permission(self):
+        if not self.request.user.is_authenticated:
+            return super().handle_no_permission()
+        return HttpResponse("Only superusers can access module management")
 
 
-class ModuleActionView(View):
+class ModuleActionView(LoginRequiredMixin, UserPassesTestMixin, View):
     """View to handle module actions: activate, deactivate, upgrade"""
+
+    login_url = "/admin/login/"  # Uses Django admin login
+    redirect_field_name = "next"
+
+    def test_func(self):
+        return self.request.user.is_superuser
+
+    def handle_no_permission(self):
+        if not self.request.user.is_authenticated:
+            return super().handle_no_permission()
+        return HttpResponse("Only superusers can access module management")
 
     def post(self, request, module_id):
         module = get_object_or_404(Module, id=module_id)
